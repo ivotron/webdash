4f5b10d66bd78f7a3f3794caa142085b898b5a75
Barbara Galindo Dorame <barbaragd@localhost.localdomain>
2019-06-03 23:51:37 -0700

    issue with uploading changes to issue-4

--- backend/apps/blackswan/views.py
+++ backend/apps/blackswan/views.py
@@ -1,6 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.blackswan.serializers import WorkflowExecutionSerializer
 from apps.blackswan.models import WorkflowExecution
+from apps.blackswan.serializers import ProjectSerializer
+from apps.blackswan.models import Project
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
 class WorkflowExecutionViewSet(ModelViewSet):
