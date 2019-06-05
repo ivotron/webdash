980934986291347eda8b0f34f9305d3fe0f4dd85
Bárbara Galindo <galindodorame@gmail.com>
2019-06-04 18:03:14 -0700

    Add fixtures for users, projects and executions (#6)
    to add fixtures:
    python manage.py loaddata users.json executions.json projects.json

--- backend/apps/blackswan/views.py
+++ backend/apps/blackswan/views.py
@@ -1,14 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from apps.blackswan.serializers import WorkflowExecutionSerializer
-from apps.blackswan.models import WorkflowExecution
-from apps.blackswan.serializers import ProjectSerializer
-from apps.blackswan.models import Project
+from apps.blackswan.serializers import WorkflowExecutionSerializer, \
+                                       ProjectSerializer
+from apps.blackswan.models import WorkflowExecution, Project
 
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
 
 class WorkflowExecutionViewSet(ModelViewSet):
     queryset = WorkflowExecution.objects.all()
