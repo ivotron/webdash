c9a2650f5a0c0ab048c53c9eb9459c0683c9633a
Barbara Galindo Dorame <galindodorame@gmail.com>
2019-06-25 01:46:08 -0700

    Initial profile

---
+++ frontend/src/components/user/RepositoriesComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <template v-for="project in projects">
+      <div class="md-layout md-gutter md-align-center">
+        <md-icon clas="md-layout-item md-size-100">book</md-icon>
+        <p class="md-layout-item md-size-15">{{ project.title }}</p>
+        <md-switch v-model="boolean" class="md-primary md-layout md-size-15">Primary</md-switch>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data: () => ({
+    boolean: false,
+    projects: []
+  }),
+  created () {
+    axios.get('/api/projects', {}, { headers: { 'Authorization':`Token ${this.$store.state.auth.token}` } })
+      .then(response => {
+        this.projects = response.data
+      })
+  }
+}
+</script>
+
+<style>
+</style>
