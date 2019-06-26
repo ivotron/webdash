7b59c30c0464e4318d00f3b2f8a717068a50576f
Barbara Galindo Dorame <galindodorame@gmail.com>
2019-06-26 13:13:57 -0700

    show user auth key

---
+++ frontend/src/components/user/SettingsComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <md-field>
+      <label></label>
+      <md-input v-model="password" type="password"></md-input>
+    </md-field>
+
+<script>
+  export default {
+    data: () => ({
+      initial: 'Initial Value',
+      password: 'mysecurepassword'
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+  .md-field:last-child {
+    margin-bottom: 40px;
+  }
+</style>
