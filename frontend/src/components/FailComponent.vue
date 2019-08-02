b9fab75016c4fa81222da1d8edf2b2c208553dcc
Barbara Galindo Dorame <galindodorame@gmail.com>
2019-08-01 22:29:03 -0700

    Add view for bad URL

---
+++ frontend/src/components/FailComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <md-empty-state
+      md-icon="rowing"
+      md-label="404 Bad URL"
+      md-description="Sorry, we couldn't find that page.">
+    </md-empty-state>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import {vueAuth} from './main'
 
 import NavbarComponent from '@/components/NavbarComponent.vue'
+import FailComponent from '@/components/FailComponent.vue'
 import ProjectListComponent from '@/components/project/ProjectListComponent.vue'
 import ExecutionListComponent from '@/components/workflowexec/ExecutionListComponent.vue'
 import ExecutionComponent from '@/components/workflowexec/ExecutionComponent.vue'
@@ -99,6 +100,11 @@ const routes = [
             component: WorkflowComponent
           }
         ]
+      },
+      {
+        path: '*',
+        name: '404',
+        component: FailComponent
       }
     ]
   },
