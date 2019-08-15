cb0317dc79bbdb4aedfc9e3b38194727cba78136
Barbara Galindo Dorame <galindodorame@gmail.com>
2019-08-14 20:11:13 -0700

    initial home

---
+++ frontend/src/components/HomeComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-content class="main">
+      <div class="md-layout md-gutter md-alignment-center-space-beteween hero">
+        <span class="md-title md-layout-item md-size-100">Welcome to Blackswan</span>
+        <span class="md-subheading md-layout-item md-size-100">We keep a watch on your projects</span>
+      </div>
+    </md-content>
+    <md-content class="mission">
+
+    </md-content>
+    <md-content class="how-to">
+
+    </md-content>
+    <md-content class="contact-us">
+
+    </md-content>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.main {
+  display: flex;
+  justify-content: center;
+}
+.hero {
+  justify-content: flex-start;
+  max-width: 600px;
+  width: 100%;
+}
+</style>
